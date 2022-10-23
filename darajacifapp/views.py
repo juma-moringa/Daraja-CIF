07269ac5e28b944e8f3a2dee49e6e953ee1c8c13
jLuseno161 <joyluseno2@gmail.com>
2022-10-23 11:27:13 +0300

    navbar design

--- darajacifapp/views.py
+++ darajacifapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the Daraja-cif.")
+    return render(request,'index.html')
