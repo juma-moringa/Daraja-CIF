7bbd5537324d70c0f4f2c4f1b70bb8aa53ce5cbb
jLuseno161 <joyluseno2@gmail.com>
2022-11-17 00:01:58 +0300

    update home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-    $('.navbar li a').css({ "color": "white" });
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.navbar li a').css({ "color": "black" });
-            $(".navbar").removeClass("navbar-light bg-transparent").addClass("navbar-light bg-light");
+// $(document).ready(function () {
+//     $('.navbar li a').css({ "color": "white" });
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 0) {
+//             $('.navbar li a').css({ "color": "black" });
+//             $(".navbar").removeClass("navbar-light bg-transparent").addClass("navbar-light bg-light");
 
-        } else {
-            $('.navbar li a').css({ "color": "white" });
-            $(".navbar").removeClass("navbar-light bg-light").addClass("navbar-light bg-transparent");
+//         } else {
+//             $('.navbar li a').css({ "color": "white" });
+//             $(".navbar").removeClass("navbar-light bg-light").addClass("navbar-light bg-transparent");
 
-        }
-    });
-});
+//         }
+//     });
+// });
